d1fe81d53cac5c29adde672ce583292fc6d2157b
David Celis <david@davidcelis.com>
2012-09-21 14:21:53 -0700

    "Initial" commit.
    The first attempt at a rewrite ended up being horribly far removed from
    what I've decided I want to do with this project. Here's to a fresh
    start.
    
    Signed-off-by: David Celis <david@davidcelis.com>

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+gem 'bcrypt-ruby'
+
+gem 'recommendable'
+gem 'sidekiq'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'pjax-rails'
---
+++ config/initializers/recommendable.rb
@@ -0,0 +1,28 @@
+require "redis"
+
+# Recommendable requires a connection to a running redis-server. Either create
+# a new instance based on a host/port or UNIX socket, or pass in an existing
+# Redis client instance.
+Recommendable.redis = Redis.new(:host => "localhost", :port => 6379)
+
+# Connect to Redis via a UNIX socket instead
+# Recommendable.redis = Redis.new(:sock => "")
+
+# Tell Redis which database to use (usually between 0 and 15). The default of 0
+# is most likely okay unless you have another application using that database.
+# Recommendable.redis.select "0"
+
+# If using Sidekiq, uncomment the following or place it in config/initializers/sidekiq.rb:
+# Sidekiq.configure_server do |config| 	
+#   require 'sidekiq/middleware/server/unique_jobs'
+#   config.server_middleware do |chain|
+#     chain.add Sidekiq::Middleware::Server::UniqueJobs
+#   end	 	
+# end
+# 
+# Sidekiq.configure_client do |config|	 	
+#   require 'sidekiq/middleware/client/unique_jobs'
+#   config.client_middleware do |chain|
+#     chain.add Sidekiq::Middleware::Client::UniqueJobs
+#   end
+# end
