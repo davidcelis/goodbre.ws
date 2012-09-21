0eff5f440c738388bf740a04929b7e4abba99e0d
David Celis <david@davidcelis.com>
2012-09-21 15:17:42 -0700

    Add Authentication-related helpers to ApplicationController
    Signed-off-by: David Celis <david@davidcelis.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  force_ssl
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def ensure_signed_in!
+    redirect_to sign_in_path, :alert => 'Please sign in to continue.' unless signed_in?
+  end
+
+  helper_method :current_user, :signed_in?
 end
