19ccb94c2c95c953e5e51ddab219439247710ff4
David Celis <david@davidcelis.com>
2012-09-21 14:44:34 -0700

    Install Recommendable
    Signed-off-by: David Celis <david@davidcelis.com>

---
+++ db/migrate/20120921214202_create_likes.recommendable.rb
@@ -0,0 +1,18 @@
+# This migration comes from recommendable (originally 20120124193723)
+class CreateLikes < ActiveRecord::Migration
+  def up
+    create_table :recommendable_likes do |t|
+      t.references :user
+      t.references :likeable, :polymorphic => true
+      t.timestamps
+    end
+    
+    add_index :recommendable_likes, :likeable_id
+    add_index :recommendable_likes, :likeable_type
+    add_index :recommendable_likes, [:user_id, :likeable_id, :likeable_type], :unique => true, :name => "user_like_constraint"
+  end
+
+  def down
+    drop_table :recommendable_likes
+  end
+end
