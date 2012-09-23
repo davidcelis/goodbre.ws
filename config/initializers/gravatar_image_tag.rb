c676e012457c7668eb86baba873a753bde085089
David Celis <david@davidcelis.com>
2012-09-23 09:42:59 -0700

    Gravatar
    Signed-off-by: David Celis <david@davidcelis.com>

---
+++ config/initializers/gravatar_image_tag.rb
@@ -0,0 +1,8 @@
+GravatarImageTag.configure do |config|
+  config.default_image           = 'http://goodbre.ws/assets/user.png'
+  config.filetype                = 'png'
+  config.include_size_attributes = true
+  config.rating                  = 'G'
+  config.size                    = 150
+  config.secure                  = false
+end
