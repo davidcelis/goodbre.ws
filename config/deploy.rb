95eaa9e344ef723d30449c919b975428a3af8e2e
David Celis <david@davidcelis.com>
2012-09-24 00:20:15 -0700

    Capify
    Signed-off-by: David Celis <david@davidcelis.com>

---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+require 'bundler/capistrano'
+
+set :application, 'goodbre.ws'
+
+server '74.207.230.240', :web, :app, :db, :primary => true
+
+set :repository,  'git@github.com:davidcelis/goodbre.ws.git'
+set :branch, 'master'
+set :scm, :git
+
+set :user, 'david'
+set :use_sudo, false
+set :deploy_to, "/home/david/apps/#{application}"
+set :deploy_via, :remote_cache
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+after 'deploy:finalize_update', 'sensitive_files:copy'
+after 'deploy', 'deploy:cleanup'
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}" # Using unicorn as the app server
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_ini.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/smtp.yml #{release_path}/config/smtp.yml"
+    run "ln -nfs #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
---
+++ config/unicorn.rb
@@ -0,0 +1,24 @@
+# Define your root directory
+root = "/home/david/apps/goodbre.ws/current"
+
+# Define worker directory for Unicorn
+working_directory root
+
+# Location of PID file
+pid "#{root}/tmp/pids/unicorn.pid"
+
+# Define Log paths
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+# Listen on a UNIX data socket
+listen "/tmp/unicorn.goodbre.ws.sock"
+
+# 16 worker processes for production environment
+worker_processes 16
+
+# Load rails before forking workers for better worker spawn time
+preload_app true
+
+# Restart workes hangin' out for more than 240 secs
+timeout 240
