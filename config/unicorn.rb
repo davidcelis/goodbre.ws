5c70b614235c3feef8438add2ca1c9f5ff7828f9
David Celis <david@davidcelis.com>
2012-09-24 16:43:12 -0700

    Attempting to fix Unicorn
    Signed-off-by: David Celis <david@davidcelis.com>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,5 +20,13 @@ worker_processes 16
 # Load rails before forking workers for better worker spawn time
 preload_app true
 
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+  # ...
+end
+
 # Restart workers hangin' out for more than 240 secs
 timeout 240
