655a7419cef1b161a371f0a2d1ca8582a9873d9e
David Celis <david@davidcelis.com>
2012-09-24 15:07:54 -0700

    Trying SSL, I guess.
    Signed-off-by: David Celis <david@davidcelis.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  force_ssl
 
   layout :set_layout
 
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,5 +20,5 @@ worker_processes 16
 # Load rails before forking workers for better worker spawn time
 preload_app true
 
-# Restart workes hangin' out for more than 240 secs
+# Restart workers hangin' out for more than 240 secs
 timeout 240
