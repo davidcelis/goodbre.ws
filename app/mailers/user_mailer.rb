e6bb5df3d8184f006ed7c4d10b0c17cbbed60cc2
David Celis <david@davidcelis.com>
2012-09-26 10:40:29 -0700

    Goodbye Google Apps, hello Mandrill.
    Signed-off-by: David Celis <david@davidcelis.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'brewmaster@goodbre.ws'
+  default from: 'The Brewmaster <brewmaster@goodbre.ws>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
