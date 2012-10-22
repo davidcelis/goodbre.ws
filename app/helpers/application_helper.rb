2459160b5595e17f0c3107d166a47878eddeeac4
David Celis <david@davidcelis.com>
2012-10-21 19:08:07 -0700

    Speed up rendering of buttons
    Signed-off-by: David Celis <david@davidcelis.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,75 +1,80 @@
 module ApplicationHelper
-  def like_button(beer)
-    if current_user.likes?(beer)
+  def action_buttons(beer)
+    likes = current_user.likes?(beer)
+    dislikes = current_user.dislikes?(beer)
+    hides = current_user.hides?(beer)
+    bookmarks = current_user.bookmarks?(beer)
+
+    if likes
       klass  = 'btn like-button btn-success'
       action = 'unlike'
+      disabled = false
     else
       klass  = 'btn like-button'
       action = 'like'
-      disabled = true if current_user.dislikes?(beer) || current_user.hides?(beer)
+      disabled = dislikes || hides ? true : false
     end
 
-    <<-BUTTON
+    like_button = <<-BUTTON
 <button class='#{klass}' data-id='#{beer.id}' data-action='#{action}' data-original-title='#{action.capitalize}' #{'disabled' if disabled}>
   <i class='icon-thumbs-up'></i>
   <span class='like-count pull-right'>#{beer.liked_by_count}</span>
 </button>
     BUTTON
-  end
 
-  def dislike_button(beer)
-    if current_user.dislikes?(beer)
+    if dislikes
       klass  = 'btn dislike-button btn-danger'
       action = 'undislike'
+      disabled = false
     else
       klass  = 'btn dislike-button'
       action = 'dislike'
-      disabled = true if current_user.likes?(beer) || current_user.hides?(beer)
+      disabled = likes || hides ? true : false
     end
 
-    <<-BUTTON
+    dislike_button = <<-BUTTON
 <button class='#{klass}' data-id='#{beer.id}' data-action='#{action}' data-original-title='#{action.capitalize}' #{'disabled' if disabled}>
   <i class='icon-thumbs-down'></i>
   <span class='dislike-count pull-right'>#{beer.disliked_by_count}</span>
 </button>
     BUTTON
-  end
 
-  def fridge_button(beer)
-    if current_user.bookmarks?(beer)
+    if bookmarks
       klass  = 'btn fridge-button btn-primary'
       action = 'unbookmark'
       title  = 'Remove from fridge'
+      disabled = false
     else
       klass  = 'btn fridge-button'
       action = 'bookmark'
       title  = 'Put in fridge'
-      disabled = true if current_user.hides?(beer)
+      disabled = hides ? true : false
     end
 
-    <<-BUTTON
+    bookmark_button = <<-BUTTON
 <button class='#{klass}' data-id='#{beer.id}' data-action='#{action}' data-original-title='#{title}' #{'disabled' if disabled}>
   <i class='icon-bookmark'></i>
 </button>
     BUTTON
-  end
 
-  def hide_button(beer)
-    if current_user.hides?(beer)
+    if hides
       klass  = 'btn hide-button btn-warning'
       action = 'unhide'
       title  = 'Unhide'
+      disabled = false
     else
       klass  = 'btn hide-button'
       action = 'hide'
       title  = 'Hide'
-      disabled = true if current_user.likes?(beer) || current_user.dislikes?(beer) || current_user.bookmarks?(beer)
+      disabled = likes || dislikes || bookmarks ? true : false
     end
 
-    <<-BUTTON
+    hide_button = <<-BUTTON
 <button class='#{klass}' data-id='#{beer.id}' data-action='#{action}' data-original-title='#{title}' #{'disabled' if disabled}>
   <i class='icon-eye-close'></i>
 </button>
     BUTTON
+
+    [like_button, dislike_button, bookmark_button, hide_button].join("\n")
   end
 end
