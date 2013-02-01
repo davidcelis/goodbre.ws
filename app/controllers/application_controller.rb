c525061f175b9c9214a83a7b1b1532f0f5122313
David Celis <me@davidcel.is>
2013-02-01 10:59:58 -0800

    Handle unverified requests.
    Signed-off-by: David Celis <me@davidcel.is>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,32 +4,40 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  protected
+
+    def handle_unverified_request
+      super
+      cookies.delete(:auth_token)
+      @current_user = nil
+    end
+
   private
 
-  def current_user
-    @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
-  end
-
-  def signed_in?
-    !!current_user
-  end
-
-  def ensure_signed_in!
-    redirect_to sign_in_account_path, :alert => 'Please sign in to continue.' unless signed_in?
-  end
-
-  # lol
-  def ensure_admin!
-    current_user.username == 'davidcelis'
-  end
-
-  helper_method :current_user, :signed_in?
-
-  def set_layout
-    if request.headers['X-PJAX']
-      'pjax'
-    else
-      'application'
+    def current_user
+      @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
     end
-  end
+
+    def signed_in?
+      !!current_user
+    end
+
+    def ensure_signed_in!
+      redirect_to sign_in_account_path, :alert => 'Please sign in to continue.' unless signed_in?
+    end
+
+    # lol
+    def ensure_admin!
+      current_user.username == 'davidcelis'
+    end
+
+    helper_method :current_user, :signed_in?
+
+    def set_layout
+      if request.headers['X-PJAX']
+        'pjax'
+      else
+        'application'
+      end
+    end
 end
