836903336a5a0e9fbd4e02302d41899d568c91ca
David Celis <me@davidcel.is>
2013-02-04 13:44:56 -0800

    Allow both G and PG rated Gravatars
    Signed-off-by: David Celis <me@davidcel.is>

--- config/initializers/gravatar_image_tag.rb
+++ config/initializers/gravatar_image_tag.rb
@@ -2,7 +2,7 @@ GravatarImageTag.configure do |config|
   config.default_image           = 'mm'
   config.filetype                = 'png'
   config.include_size_attributes = true
-  config.rating                  = 'G'
+  config.rating                  = 'pg'
   config.size                    = 300
   config.secure                  = true
 end
